fbea1e4490aac7f003aeaed98711b411452fcfa5
VuzLe <tle63917@gmail.com>
2026-06-07 12:16:02 +0700

    feat(core-database): enhance Connect function with context timeout and error handling

--- internal/core/database/postgres.go
+++ internal/core/database/postgres.go
@@ -2,13 +2,27 @@ package database
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func Connect(databaseURL string) (*pgxpool.Pool, error) {
-	return pgxpool.New(
+	ctx, cancel := context.WithTimeout(
 		context.Background(),
-		databaseURL,
+		5*time.Second,
 	)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
