3d75418dc8132c91b6a09ca040647ab688402b41
VuzLe <tle63917@gmail.com>
2026-06-07 12:12:11 +0700

    feat(config): update Load function to return error and validate configuration

--- internal/core/config/config.go
+++ internal/core/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -11,11 +12,29 @@ type Config struct {
 	DatabaseURL string
 }
 
-func Load() *Config {
+func Load() (*Config, error) {
 	_ = godotenv.Load()
 
-	return &Config{
+	cfg := Config{
 		Port:        os.Getenv("PORT"),
 		DatabaseURL: os.Getenv("DATABASE_URL"),
 	}
+
+	if err := validateConfig(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func validateConfig(config *Config) error {
+	if config.Port == "" {
+		return errors.New("missing PORT")
+	}
+
+	if config.DatabaseURL == "" {
+		return errors.New("missing DATABASE_URL")
+	}
+
+	return nil
 }
