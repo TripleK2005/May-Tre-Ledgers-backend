319bef6386dd86aefb3d4078fa353b9f9a853797
VuzLe <tle63917@gmail.com>
2026-06-07 11:29:57 +0700

    refactor(response): make Response type generic and update field formatting

--- internal/core/response/error.go
+++ internal/core/response/error.go
@@ -3,7 +3,7 @@ package response
 import "github.com/gin-gonic/gin"
 
 func Error(c *gin.Context, status int, message string) {
-	c.JSON(status, Response{
+	c.JSON(status, Response[any]{
 		Success: false,
 		Message: message,
 	})
--- internal/core/response/success.go
+++ internal/core/response/success.go
@@ -2,8 +2,8 @@ package response
 
 import "github.com/gin-gonic/gin"
 
-func Success(c *gin.Context, status int, message string, data interface{}) {
-	c.JSON(status, Response{
+func Success[T any](c *gin.Context, status int, message string, data T) {
+	c.JSON(status, Response[T]{
 		Success: true,
 		Message: message,
 		Data:    data,
