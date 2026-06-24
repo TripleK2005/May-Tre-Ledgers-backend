134ceea19199587a1fb8d51ac0178a0b75ac545b
VuzLe <tle63917@gmail.com>
2026-06-24 21:08:33 +0700

    feat(user): implement user service and handler with GetByID method

---
+++ internal/modules/user/handler.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"may-tre-ledger-be/internal/core/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
+
+func (h *Handler) GetByID(c *gin.Context) {
+	id := c.Param("id")
+	user, err := h.service.GetByID(
+		c.Request.Context(),
+		id,
+	)
+
+	if err != nil {
+		response.Error(c, http.StatusNotFound, "user not found")
+		return
+	}
+
+	resp := UserResponse{
+		ID:        user.ID.String(),
+		Username:  user.Username,
+		Email:     user.Email,
+		FullName:  user.FullName,
+		Role:      user.RoleName,
+		IsActive:  user.IsActive,
+		CreatedAt: user.CreatedAt,
+	}
+
+	response.Success(c, http.StatusOK, "get user success", resp)
+}
